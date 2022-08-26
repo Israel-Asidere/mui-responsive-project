30a76a1477fbf0beeb42865145ab652a4ed000d1
Israel-Asidere <52455381+Israel-Asidere@users.noreply.github.com>
2022-08-26 13:35:47 +0100

    Add button not displaying, night mode working fine

--- src/App.js
+++ src/App.js
@@ -2,19 +2,30 @@ import { Sidebar } from './components/Sidebar'
 import { Feed } from './components/Feed'
 import { RightBar } from './components/RightBar'
 import { Box } from '@mui/system'
-import { Stack } from '@mui/material'
+import { createTheme, Stack, ThemeProvider } from '@mui/material'
 import { Navbar } from './components/Navbar'
+import { Add } from '@mui/icons-material'
+import { useState } from 'react'
 
 function App () {
+  const [mode, setMode] = useState('light')
+  const darkTheme = createTheme({
+    palette: {
+      mode: mode
+    }
+  })
   return (
-    <Box>
-      {/* <Navbar /> */}
-      <Stack direction='row' spacing={2} justifyContent='space-between'>
-        <Sidebar />
-        <Feed />
-        <RightBar />
-      </Stack>
-    </Box>
+    <ThemeProvider theme={darkTheme}>
+      <Box bgcolor={'background.default'} color={'text.primary'}>
+        <Navbar />
+        <Stack direction='row' spacing={2} justifyContent='space-between'>
+          <Sidebar setMode={setMode} mode={mode} />
+          <Feed />
+          <RightBar />
+        </Stack>
+        <Add />
+      </Box>
+    </ThemeProvider>
   )
 }
 
